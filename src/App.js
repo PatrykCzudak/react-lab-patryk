79edd5c5b56a6ef4690db9567a9fd4efda19b802
Student <student@d17-327e-pc-05.ipa.ii.agh.edu.pl>
2025-01-12 13:26:22 +0100

    first changes - reaktywność

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [title, setTitle] = useState('Wall-E');
+
+  function handleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  let message;
+  if (title.length < 5) {
+      message = "Tutuł jest za krótki. Nagrywają takie filmy?";
+  } else if (title.length < 15) {
+      message = "Tytuł jest ekstra, w sam raz na plakat przed kinem!";
+  } else {
+      message = "Tytuł jest za długi, nikt tego nie zapamięta.";
+  }
+      title.length > 0 && <div>{message}</div>
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <h1>My favourite movies to watch</h1>
+          <h2>My favourite movie for today is {title}</h2>
+          <div>{message}</div>
+          <input type="text" value={title} onChange={handleChange}/>
+          <button type="button">Pokaż tytuł filmu</button> 
+      </div>
   );
 }
 
