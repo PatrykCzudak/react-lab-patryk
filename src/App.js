7e84f6a1ce80c59ad24460ab73b5ba6c7a37ec06
Student <student@d17-327e-pc-05.ipa.ii.agh.edu.pl>
2025-01-12 14:16:13 +0100

    Dodawanie filmów

--- src/App.js
+++ src/App.js
@@ -1,47 +1,65 @@
 import {useState} from "react";
+import "milligram";
 
 function App() {
-  const [title, setTitle] = useState('Wall-E');
 
-  function handleChange(event) {
+  const movies_init = [
+    {title: "Wall-E", year: "1999"},
+    {title: "Pulp Fiction", year: "1998"},
+    {title: "Matrix", year: "1997"},
+    {title: "1670", year: "1996"},
+  ];
+
+  const [moviesList, setMoviesList] = useState(movies_init)
+  const [title, setTitle] = useState('Wall-E');
+  const [year, setYear] = useState([]);
+
+  function TitleChange(event) {
     setTitle(event.target.value);
+  };
+
+  function YearChange(event) {
+    setYear(event.target.value);
+  };
+
+  function displayList(movie) {
+    return (
+      <li key={movie.title}>{movie.title} ({movie.year})</li>
+    )
   }
 
-  let message;
-  if (title.length < 5) {
-      message = "Tutuł jest za krótki. Nagrywają takie filmy?";
-  } else if (title.length < 15) {
-      message = "Tytuł jest ekstra, w sam raz na plakat przed kinem!";
-  } else {
-      message = "Tytuł jest za długi, nikt tego nie zapamięta.";
-  };
+  function handleButton() {
+    let message;
+    if (year === "") {
+      message = "Add year!!!";
+      return alert(message)
+    }
 
-  let length_mess;
-  if (title.length > 1) { 
-    length_mess = <div>{message}</div>;
-    
-  };
+    if (title.length < 4) {
+        message = "Title is to short.";
+        return alert(message)
+    } else if (title.length < 20) {
+        const newList = moviesList.concat({ title, year });
+        setMoviesList(newList);
+    } else {
+        message = "Title is to long";
+        return alert(message)
+    };
+  }
 
-  const array1 = [1,4,9,16];
   
-  const map1 = array1.map((x) => x*2);
-
-  const movies = [
-    {title: "Wall-E"},
-    {title: "Pulp Fiction"},
-    {title: "Matrix"},
-    {title: "1670"},
-  ];
 
   return (
       <div>
-          <h1>My favourite movies to watch {map1}</h1>
+          <h1>My favourite movies to watch</h1>
           <h2>Titles</h2>
-          <ul>{movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}</ul>
-          <h2>My favourite movie for today is {title}</h2>
-          <div>{length_mess}</div>
-          <input type="text" value={title} onChange={handleChange}/>
-          <button type="button" onClick={() => alert(title)}>Pokaż tytuł filmu</button> 
+          <ul>{moviesList.map(displayList)}</ul>
+          <h1>Add Movie {title} ({year})</h1>
+          <h3>Title</h3>
+          <input type="text" value={title} onChange={TitleChange}/>
+          <h3>Year</h3>
+          <input type="text" value={year} onChange={YearChange}/>
+          <button type="button" onClick={handleButton}>Add film</button> 
       </div>
   );
 }
