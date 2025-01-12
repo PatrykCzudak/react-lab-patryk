b0df0efb5e807483e2f31f17e8154fadf44e178f
Student <student@d17-327e-pc-05.ipa.ii.agh.edu.pl>
2025-01-12 13:39:17 +0100

    Interakcje i listy elementów

--- src/App.js
+++ src/App.js
@@ -14,17 +14,34 @@ function App() {
       message = "Tytuł jest ekstra, w sam raz na plakat przed kinem!";
   } else {
       message = "Tytuł jest za długi, nikt tego nie zapamięta.";
-  }
-      title.length > 0 && <div>{message}</div>
+  };
 
+  let length_mess;
+  if (title.length > 1) { 
+    length_mess = <div>{message}</div>;
+    
+  };
+
+  const array1 = [1,4,9,16];
+  
+  const map1 = array1.map((x) => x*2);
+
+  const movies = [
+    {title: "Wall-E"},
+    {title: "Pulp Fiction"},
+    {title: "Matrix"},
+    {title: "1670"},
+  ];
 
   return (
       <div>
-          <h1>My favourite movies to watch</h1>
+          <h1>My favourite movies to watch {map1}</h1>
+          <h2>Titles</h2>
+          <ul>{movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}</ul>
           <h2>My favourite movie for today is {title}</h2>
-          <div>{message}</div>
+          <div>{length_mess}</div>
           <input type="text" value={title} onChange={handleChange}/>
-          <button type="button">Pokaż tytuł filmu</button> 
+          <button type="button" onClick={() => alert(title)}>Pokaż tytuł filmu</button> 
       </div>
   );
 }
